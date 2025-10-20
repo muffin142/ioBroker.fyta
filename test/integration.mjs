b160997fca8f3fd56ead0e1b5fec4bd92c3f9ab6
muffin142 <muffin142@outlook.com>
2025-10-20 15:07:05 +0200

    Repaired test:integration

--- test/integration.mjs
+++ test/integration.mjs
@@ -2,8 +2,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { tests } from "@iobroker/testing";
 
+// Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Validate the package files
 tests.packageFiles(path.join(__dirname, ".."));
